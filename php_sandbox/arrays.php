b0e9426f86f94ac0676617df4495ee034fcc5e94
deastman <diana.eastman@gmail.com>
2012-10-03 17:32:39 -0400

    Committing Framework to Git

---
+++ php_sandbox/arrays.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Arrays</title>
+	</head>
+	<body>
+	<p>Arrays are numbered starting from zero. If we want to know what the first number of a particular array is (in this case), we would need to specify that we want zero.<br> 
+	<?php $array1 = array(4,8,15,16,23,42);?>
+	<?php echo $array1[1];?>
+	<?php $array2 = array(6, "fox", "dog", array("x", "y", "z"));?>
+	<?php echo $array2[3][1];?>
+	
+	
+	
+	
+	</body>
+	
+</html>
