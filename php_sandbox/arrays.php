a0a7d154838bdb2980e2e97ad82f6fc633968730
deastman <diana.eastman@gmail.com>
2012-10-04 11:47:24 -0400

    Configure framework

--- php_sandbox/arrays.php
+++ php_sandbox/arrays.php
@@ -3,7 +3,7 @@
 		<title>Arrays</title>
 	</head>
 	<body>
-	<p>Arrays are numbered starting from zero. If we want to know what the first number of a particular array is (in this case), we would need to specify that we want zero.<br> 
+	<p>Array positions are numbered starting from zero. If we want to know what the first number of a particular array is (in this case, 4), we would need to specify that we want position zero.<br> 
 	<?php $array1 = array(4,8,15,16,23,42);?>
 	<?php echo $array1[1];?>
 	<?php $array2 = array(6, "fox", "dog", array("x", "y", "z"));?>
