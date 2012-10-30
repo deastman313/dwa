6eb9f6d1dc9b0d558697e6d65bf384b517e46d69
deastman <diana.eastman@gmail.com>
2012-10-30 11:01:08 -0400

    Edits to nav -- NEED TO UPDATE

--- p2.eastmaninteractive.com/views/_v_template.php
+++ p2.eastmaninteractive.com/views/_v_template.php
@@ -1,22 +1,53 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title><?=@$title; ?></title>
+	<meta charset="utf-8">
+	<!-- CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/styles.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700|PT+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
+       <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-	
+
 	<!-- JS -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.8/jquery.validate.min.js"></script>
+    <script type="text/javascript"> $(document).ready(function(){ $("#subForm").validate(); }); </script>
 				
 	<!-- Controller Specific JS/CSS -->
 	<?php echo @$client_files; ?>
-	
 </head>
-
 <body>	
 
+<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="brand" href="#">P!NG</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+            	<?=$nav;?> 
+                <?=$links;?>
+			</ul>                
+          </div><!--/.nav-collapse -->
+     	</div>
+      </div>
+    </div>
+   
+
+	 <div class="container">
 	<?=$content;?> 
+
+	
+
+	<footer><p>&copy; P!NG 2012</p></footer>
+  </div>  
 
 </body>
 </html>
