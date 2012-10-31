a09ddba5097e2fb2344e9df9c6d74f54b4f2233e
deastman <diana.eastman@gmail.com>
2012-10-31 13:17:56 -0400

    Moved posts/index to dashboard view

--- p2.eastmaninteractive.com/controllers/c_posts.php
+++ p2.eastmaninteractive.com/controllers/c_posts.php
@@ -12,28 +12,11 @@ class posts_controller extends base_controller {
 		
 	}
 	
-	public function index() {
+/*public function index() {
 
-	# Set up view
-	$this->template->content = View::instance('v_posts_index');
-	$this->template->title   = "Posts";
-
-	# Build our query
-	$q = "SELECT * 
-		FROM posts
-		JOIN users USING (user_id)";
-
-	# Run our query, grabbing all the posts and joining in the users	
-	$posts = DB::instance(DB_NAME)->select_rows($q);
-
-	# Pass data to the view
-	$this->template->content->posts = $posts;
-
-	# Render view
-	echo $this->template;
-
+	
 }
-
+*/
 public function users() {
 
 	# Set up the view
