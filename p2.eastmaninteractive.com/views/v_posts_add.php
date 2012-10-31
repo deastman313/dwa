7bfa99df2d39851592994f811d2c0fd2335fcf3c
deastman <diana.eastman@gmail.com>
2012-10-31 15:49:22 -0400

    Changed view to add a "success" method after a post has been added.

--- p2.eastmaninteractive.com/views/v_posts_add.php
+++ p2.eastmaninteractive.com/views/v_posts_add.php
@@ -1,3 +1,10 @@
+<? if($add): ?>
+		<div class="alert alert-success">
+		Your post has been added.
+    	</div>
+        <a href="/users/dashboard"><button type="button" class="btn">Add Another Post</button></a>
+		<br>
+<? else: ?>
 <form method='POST' action='/posts/p_add'>
 
 	<strong>What's up?</strong><br>
@@ -5,6 +12,9 @@
 	<textarea name='content' id="inputPost" placeholder="Write something..."></textarea>
 
 	<br><br>
-	<input type='submit'>
+	<button type='submit' class="btn">Submit</button>
 
 </form>
+
+
+<? endif; ?>
