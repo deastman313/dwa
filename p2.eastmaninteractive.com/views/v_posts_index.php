890ecc0edcc3e7b8568a5984a4f0dfcf75d67446
deastman <diana.eastman@gmail.com>
2012-10-31 15:49:42 -0400

    Reformatted UNIX timestamp using Time library

--- p2.eastmaninteractive.com/views/v_posts_index.php
+++ p2.eastmaninteractive.com/views/v_posts_index.php
@@ -2,8 +2,10 @@
 <? foreach($posts as $post): ?>
 
     <div class="well">
-    <h4><?=$post['first_name']?> <?=$post['last_name']?> posted on <?=$post['created']?>:</h4>
+    <h4><?=$post['first_name']?> <?=$post['last_name']?> posted <?=Time::time_ago ($post['modified'], $dateto=-1)?>:</h4>
 	<?=$post['content']?>
 	</div>
 	
 <? endforeach; ?>
+
+
