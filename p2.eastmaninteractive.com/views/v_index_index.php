f7280a6415f3fa4a6232cd4ba397b913b4806472
deastman <diana.eastman@gmail.com>
2012-10-31 13:18:25 -0400

    Fixed view to show subviews

--- p2.eastmaninteractive.com/views/v_index_index.php
+++ p2.eastmaninteractive.com/views/v_index_index.php
@@ -3,13 +3,13 @@
         <p>P!NG is a hub for the developer community to share snippets of code, links, and resources. </p>
         <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
       </div>
-
       <!-- Example row of columns -->
       <div class="row">
-       <?=$subview?>
-        
-        <div class="span6">
-     
-         
+       <div class="span6">
+         <?=$login?>
         </div>
+      	<div class="span6">
+         <?=$signup?>
+        </div>
+       
 </div>
