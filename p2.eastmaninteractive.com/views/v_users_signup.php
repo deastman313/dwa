a122effef7f52b990f8ad013cf44018671ab5beb
deastman <diana.eastman@gmail.com>
2012-10-31 09:33:00 -0400

    Sign up view

--- p2.eastmaninteractive.com/views/v_users_signup.php
+++ p2.eastmaninteractive.com/views/v_users_signup.php
@@ -1,21 +1,15 @@
-<form method='POST' action='/users/p_signup'>
-
-	First Name<br>
-	<input type='text' name='first_name'>
-	<br><br>
-	
-	Last Name<br>
-	<input type='text' name='last_name'>
-	<br><br>
-
-	Email<br>
-	<input type='text' name='email'>
-	<br><br>
-	
-	Password<br>
-	<input type='password' name='password'>
-	<br><br>
-	
-	<input type='submit'>
-
-</form> 
+<div class="span6">
+        	<h2>New to P!NG?</h2>
+				<form method='POST' action='/users/p_signup' id="subForm">
+                	<legend>Sign up!</legend>
+    				<label class="control-label" for="inputFirstName">First Name</label>
+					<input type="text" id="inputFirstName" placeholder="First Name" name="first_name" class="required">
+                    <label class="control-label" for="inputLastName">Last Name</label>
+					<input type="text" id="inputLastName" placeholder="Last Name" name="last_name">
+					<label class="control-label" for="inputEmail">Email</label>
+					<input type="text" id="inputEmail" placeholder="Email" name="email" class="email required">
+    				<label class="control-label" for="inputPassword">Password</label>
+					<input type="password" id="inputPassword" placeholder="Password"  name="password" class="required">
+					<input type='submit'>
+				</form> 
+</div>
