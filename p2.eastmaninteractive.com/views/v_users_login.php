9307c59928c186078f2492348ce6a4b364d9c162
deastman <diana.eastman@gmail.com>
2012-10-31 13:19:33 -0400

    Updated view fragment

--- p2.eastmaninteractive.com/views/v_users_login.php
+++ p2.eastmaninteractive.com/views/v_users_login.php
@@ -1,18 +1,14 @@
-
-
-
-<form method='POST' action='/users/p_login'>
-
-	Email<br>
-	<input type='text' name='email'>
-	
-	<br><br>
-	
-	Password<br>
-	<input type='password' name='password'>
-
-	<br><br>
-	
-	<input type='submit'>
-
-</form>
+	<h2>Already have an account? </h2>
+				<form method='POST' action='/users/p_login'>
+                	<legend>Log in</legend>
+            		<label class="control-label" for="inputEmail">Email</label>
+            		<input type="text" id="inputEmail" placeholder="Email" name="email">
+            		<label class="control-label" for="inputPassword">Password</label>
+            		<input type="password" id="inputPassword" placeholder="Password"  name="password">
+                    
+                    <label class="control-label" for="inputRemember">Remember Me</label>
+            		<input type="checkbox" id="inputRemember" placeholder="Remember"  name="remember_user">
+                    
+                    <br /><br />
+             		<input type='submit'>
+    			</form>
