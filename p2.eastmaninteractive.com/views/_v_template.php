b3de10de39d10fb5e650e6ac5e7256720d3c3ee7
deastman <diana.eastman@gmail.com>
2012-10-31 09:32:12 -0400

    Adding JS to display current page

--- p2.eastmaninteractive.com/views/_v_template.php
+++ p2.eastmaninteractive.com/views/_v_template.php
@@ -13,13 +13,23 @@
         padding-bottom: 40px;
       }
     </style>
-
-
 	<!-- JS -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
     <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.8/jquery.validate.min.js"></script>
     <script type="text/javascript"> $(document).ready(function(){ $("#subForm").validate(); }); </script>
+    <script type='text/javascript'>
+	$(function(){
+		var url = window.location.pathname, 
+        urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); 
+        $('#menu a').each(function(){
+            if(urlRegExp.test(this.href.replace(/\/$/,''))){
+                $(this).addClass('active');
+            }
+        });
+
+});
+</script>
 				
 	<!-- Controller Specific JS/CSS -->
 	<?php echo @$client_files; ?>
@@ -27,27 +37,38 @@
 <body>	
 
 <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
+	<div class="navbar-inner">
         <div class="container">
-          <a class="brand" href="#">P!NG</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-            	<?=$nav;?> 
-                <?=$links;?>
-			</ul>                
-          </div><!--/.nav-collapse -->
-     	</div>
-      </div>
+          	<a class="brand" href="/index">P!NG</a>
+          		<div id="menu">
+          			<div class="nav-collapse collapse">
+          			<? if($user): ?>
+						<ul class="nav">
+            				<li><a href="/index">Home</a></li>
+                			<li><a href="/users/dashboard">Dashboard</a></li>
+                			<li><a href="/posts">View Posts</a></li>
+                			<li><a href="/posts">View Posts</a></li>
+                
+             			</ul>
+          			</div><!--/.nav-collapse -->
+      			</div>
+      		</div>
+    	</div>
     </div>
-   
+    <? else: ?>
+    				</div><!--/.nav-collapse -->
+        		</div>
+      		</div>
+    	</div>
+    </div>
+    <? endif; ?>
 
 	 <div class="container">
-	<?=$content;?> 
+		<?=$content;?> 
 
 	
 
 	<footer><p>&copy; P!NG 2012</p></footer>
   </div>  
-
 </body>
 </html>
