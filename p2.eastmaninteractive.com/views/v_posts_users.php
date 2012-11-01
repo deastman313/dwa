5c33554afbc8df57401c783cd69c7ef7d1d83c9d
deastman <diana.eastman@gmail.com>
2012-11-01 08:35:30 -0400

    Post view update

--- p2.eastmaninteractive.com/views/v_posts_users.php
+++ p2.eastmaninteractive.com/views/v_posts_users.php
@@ -3,20 +3,23 @@
 <form method='POST' action='/posts/p_follow'>
 		
 	<? foreach($users as $user): ?>
-	
-		<!-- Print this user's name -->
-		<?=$user['first_name']?> <?=$user['last_name']?>
-		
-		<!-- If there exists a connection with this user, show a unfollow link -->
-		<? if(isset($connections[$user['user_id']])): ?>
-			<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+		<div class="row">
+        	<div class="span3">
+            <!-- Print this user's name -->
+            <?=$user['first_name']?> <?=$user['last_name']?>
+            </div>
+			<div class="span2">
+            <!-- If there exists a connection with this user, show a unfollow link -->
+            <? if(isset($connections[$user['user_id']])): ?>
+                <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
 		
 		<!-- Otherwise, show the follow link -->
-		<? else: ?>
-			<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-		<? endif; ?>
-	
+				<? else: ?>
+                    <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+                <? endif; ?>
+			</div>
 		<br><br>
+        </div>
 	
 	<? endforeach; ?>
 	
