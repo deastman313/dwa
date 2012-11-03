8ee4b6458450afedb9fb8dc38e48290efc8bc28d
deastman <diana.eastman@gmail.com>
2012-11-03 16:29:41 -0400

    Updated query to pull just one row

--- p2.eastmaninteractive.com/views/v_users_profile.php
+++ p2.eastmaninteractive.com/views/v_users_profile.php
@@ -1,23 +1,26 @@
-<h1><?= $profile_contents[0]['first_name']?> <?= $profile_contents[0]['last_name']?>'s Profile </h1>
+<h1><?= $profile_contents['first_name']?> <?= $profile_contents['last_name']?>'s Profile </h1>
 
-<? if($profile_contents[0]['user_id']!=$user->user_id): ?>
+<? if($profile_contents['user_id']!=$user->user_id): ?>
 
-This is not your profile.
-
-<? else: ?>
+        Member since: <?= $profile_contents['account_created']?><br />
+        Location: <?= $profile_contents['location']?><br />
+        Interests: <?= $profile_contents['interests']?><br />
+        Github Repository: <?= $profile_contents['github']?><br />
+	
+<? else:?>
 
 <h2>Update Your Profile</h2>
 
 	<form method='POST' action='/users/p_profile'>
     
         <label class="control-label" for="inputLocation">Location</label>
-        <input type="text" id="inputLocation" placeholder="<?= $profile_contents[0]['location']?>" name="location">
+        <input type="text" id="inputLocation" placeholder="<?= $profile_contents['location']?>" name="location">
         
         <label class="control-label" for="inputInterests">Interests</label>
-        <textarea rows="3" id="inputInterests" placeholder="<?= $profile_contents[0]['interests']?>" name="interests"></textarea>
+        <textarea rows="3" id="inputInterests" placeholder="<?= $profile_contents['interests']?>" name="interests"></textarea>
                     
         <label class="control-label" for="inputGithub">Link to Your Github Repository</label>
-        <input type="text" id="inputGithub" placeholder="<?= $profile_contents[0]['github']?>" name="github">
+        <input type="text" id="inputGithub" placeholder="<?= $profile_contents['github']?>" name="github">
         
         
         <input type="hidden" value="0" name="visibility" />
@@ -31,7 +34,7 @@ This is not your profile.
     
     
 	
-	<strong>Visibility:</strong> <? if($profile_contents[0]['visibility']=1) :?>Public <? else:?>Private<? endif?>
+	<strong>Visibility:</strong> <? if($profile_contents['visibility']=1) :?>Public <? else:?>Private<? endif?>
 
 <? endif;?>
              
