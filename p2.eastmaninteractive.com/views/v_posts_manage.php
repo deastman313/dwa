d78d18f7670c7ec3c04732da17932bff21a0a0e4
deastman <diana.eastman@gmail.com>
2012-11-03 14:54:14 -0400

    Added alert message to display when users have no posts to manage

--- p2.eastmaninteractive.com/views/v_posts_manage.php
+++ p2.eastmaninteractive.com/views/v_posts_manage.php
@@ -1,15 +1,25 @@
-<h2>Manage Your P!NGs</h2>
+<h1>Manage Your P!NGs</h1>
 
-<? foreach($myposts as $mypost): ?>
+<? if(isset($noposts)):?>
+	<div class="alert alert-block">
+  		<p>Uh-oh... you have no P!NGs left! Return to your dashboard to compose a new P!NG?</p>
+    </div>
+        <a href="/users/dashboard"><button class="btn">Take me there</button></a>
 
-    <div class="well">
-    <h4>You posted on <?= Time::display($mypost['modified'],"","") ?>:</h4>
-	<?=$mypost['content']?>
-   	<br>
-    <br>
-   	<a href="/posts/delete/<?=$mypost['post_id']?>"><button class="btn btn-danger">Delete this post</button></a>
-	</div>
-	
-<? endforeach; ?>
+<? else: ?>
+
+	<? foreach($myposts as $mypost): ?>
+        <div class="well">
+        <h4>You posted on <?= Time::display($mypost['modified'],"","") ?>:</h4>
+        <?=$mypost['content']?>
+        <br>
+        <br>
+        <a href="/posts/delete/<?=$mypost['post_id']?>"><button class="btn btn-danger">Delete this post</button></a>
+        </div> 
+    <? endforeach; ?>
+
+<? endif; ?>
+
+
 
 
