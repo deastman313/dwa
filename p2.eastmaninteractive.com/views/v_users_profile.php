7be02e60a393946cf5b86eee1f1a76d8eb998b09
deastman <diana.eastman@gmail.com>
2012-11-03 14:56:10 -0400

    Improved profile appearance -- MUST EDIT the display for other users' profiles

--- p2.eastmaninteractive.com/views/v_users_profile.php
+++ p2.eastmaninteractive.com/views/v_users_profile.php
@@ -1,5 +1,38 @@
-<h2>Your Profile </h2>
-<dl><strong>Name:</strong> <?=$user->first_name?> <?=$user->last_name?></dl>
+<h1><?= $profile_contents[0]['first_name']?> <?= $profile_contents[0]['last_name']?>'s Profile </h1>
 
+<? if($profile_contents[0]['user_id']!=$user->user_id): ?>
 
+This is not your profile.
+
+<? else: ?>
+
+<h2>Update Your Profile</h2>
+
+	<form method='POST' action='/users/p_profile'>
+    
+        <label class="control-label" for="inputLocation">Location</label>
+        <input type="text" id="inputLocation" placeholder="<?= $profile_contents[0]['location']?>" name="location">
+        
+        <label class="control-label" for="inputInterests">Interests</label>
+        <textarea rows="3" id="inputInterests" placeholder="<?= $profile_contents[0]['interests']?>" name="interests"></textarea>
+                    
+        <label class="control-label" for="inputGithub">Link to Your Github Repository</label>
+        <input type="text" id="inputGithub" placeholder="<?= $profile_contents[0]['github']?>" name="github">
+        
+        
+        <input type="hidden" value="0" name="visibility" />
+		<label class="checkbox"> Allow other users to see my profile
+        <input type="checkbox" value="1" name="visibility" />
+        </label>
+                    
+         <br /><br />
+        <button type='submit' class="btn">Submit</button>
+    	</form>
+    
+    
 	
+	<strong>Visibility:</strong> <? if($profile_contents[0]['visibility']=1) :?>Public <? else:?>Private<? endif?>
+
+<? endif;?>
+             
+
