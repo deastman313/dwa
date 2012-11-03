34d39dacabae09c0e0d63ad989cac29ec232441f
deastman <diana.eastman@gmail.com>
2012-11-03 14:53:07 -0400

    Beta 1

--- p2.eastmaninteractive.com/views/v_posts_add.php
+++ p2.eastmaninteractive.com/views/v_posts_add.php
@@ -1,20 +1,15 @@
 <? if($add): ?>
-		<div class="alert alert-success">
+    <div class="alert alert-success">
 		Your post has been added.
-    	</div>
-        <a href="/users/dashboard"><button type="button" class="btn">Add Another Post</button></a>
-		<br>
+	</div>
+    <a href="/users/dashboard"><button type="button" class="btn">Add Another Post</button></a>
+	<br>
+    
 <? else: ?>
 <form method='POST' action='/posts/p_add'>
-
-	<strong>What's up?</strong><br>
-
+	<h2>What's up?</h2><br>
 	<textarea name='content' id="inputPost" placeholder="Compose a P!NG..."></textarea>
-
 	<br><br>
 	<button type='submit' class="btn">Submit</button>
-
 </form>
-
-
 <? endif; ?>
