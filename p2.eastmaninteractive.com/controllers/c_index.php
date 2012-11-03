a67ac168694e535db8345dc701da1e200fa970d6
deastman <diana.eastman@gmail.com>
2012-11-03 14:49:02 -0400

    1. Pass restricted parameter to index to display message to users who are not logged in 2. Deleted extraneous code 3. Redirect logged in users to the dashboard to bypass the boilerplate index page

--- p2.eastmaninteractive.com/controllers/c_index.php
+++ p2.eastmaninteractive.com/controllers/c_index.php
@@ -6,7 +6,7 @@ class index_controller extends base_controller {
 		parent::__construct();
 	} 
 	
-	public function index() {
+	public function index($restricted=NULL) {
 		
 		# Any method that loads a view will commonly start with this
 		# First, set the content of the template with a view file
@@ -14,12 +14,11 @@ class index_controller extends base_controller {
 			$this->template->content = View::instance('v_index_index');
 			$this->template->content->signup = View::instance('v_users_signup');
 			$this->template->content->login = View::instance('v_users_login');
-			$this->template->content->welcome = View::instance('v_users_login');
+			$this->template->content->restricted = $restricted;
+		
 			
 		# Now set the <title> tag
 			$this->template->title = "Welcome to P!NG";
-			
-			$this->template->pageName = basename($_SERVER['PHP_SELF'], '.php');
 	
 		# If this view needs any JS or CSS files, add their paths to this array so they will get loaded in the head
 			$client_files = Array(
@@ -27,9 +26,19 @@ class index_controller extends base_controller {
 	                    );
 	    
 	    	$this->template->client_files = Utils::load_client_files($client_files);   
-	      		
-		# Render the view
+	    
+		# If no one is signed in, show the index page
+		if (!$this->user) {
+		
 			echo $this->template;
+		}
+		
+		# Otherwise, the user is already signed in and can go directly to the landing dashboard
+		else {
+		
+			Router::redirect("/users/dashboard");
+		}
+		
 
 	}
 
