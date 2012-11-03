7e29978ac4b0188904ae09a92fc9fb0520b0cb79
deastman <diana.eastman@gmail.com>
2012-11-03 14:54:29 -0400

    Beta 1

--- p2.eastmaninteractive.com/views/v_posts_users.php
+++ p2.eastmaninteractive.com/views/v_posts_users.php
@@ -5,17 +5,24 @@
 	<? foreach($users as $user): ?>
 		<div class="row">
         	<div class="span3">
-            <!-- Print this user's name -->
-            <?=$user['first_name']?> <?=$user['last_name']?>
-            </div>
+            	<div class="profiles">
+                    <!-- Print this user's name -->
+                   <?=$user['first_name']?> <?=$user['last_name']?>
+                </div>
+             </div>
+             
+             <div class="span1">
+              <a href="/users/profile/<?=$user['user_id']?>">Profile</a>
+             </div>      
+              
 			<div class="span2">
-            <!-- If there exists a connection with this user, show a unfollow link -->
+            <!-- If there exists a connection with this user, show an unfollow link -->
             <? if(isset($connections[$user['user_id']])): ?>
-                <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+            <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
 		
 		<!-- Otherwise, show the follow link -->
 				<? else: ?>
-                    <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+                   <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
                 <? endif; ?>
 			</div>
 		<br><br>
