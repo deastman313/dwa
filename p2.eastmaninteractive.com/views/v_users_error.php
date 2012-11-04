b85b86276c882b662f064c9185112be12f1c77ff
deastman <diana.eastman@gmail.com>
2012-11-03 20:24:16 -0400

    Finalizing error messages view

--- p2.eastmaninteractive.com/views/v_users_error.php
+++ p2.eastmaninteractive.com/views/v_users_error.php
@@ -1,12 +1,30 @@
-<h1>Sorry, there seems to be a problem...</h1>
+<h2>Sorry, there seems to be a problem...</h2>
+
 <!--If login=TRUE, then we know there is a problem with their password or email-->
-	<div class="alert alert-error">
+
 	<? if($login):?>
-		<h3>The email address or password you provided doesn't match our records.</h3>
+    <div class="row">
+    	<div class="span7">
+    		<div class="alert alert-error">The email address or password you provided doesn't match our records.</div>
+     	</div>
+    </div>
+    <div class="row">
+    	<div class="span2">
+        <button class="btn"><a href="/index">Return to Login</a></button>
+        </div>
 	</div>
     
 <!--Otherwise, the error comes from the signup form-->
+	
 	<? else:?>
-		<h3>An account already exists with that email address.</h3>
+    <div class="row">
+    	<div class="span5">
+    		<div class="alert alert-error">An account already exists with that email address.</div>
+        </div>
 	</div>
+    <div class="row">
+    	<div class="span3">
+    	<button class="btn"><a href="/index">Return to Sign Up</a></button>
+    	</div>
+    </div>
 <? endif;?>
