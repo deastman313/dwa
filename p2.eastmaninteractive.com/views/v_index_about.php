b9292556e90617c24ebb105cdbdd1d4088ab1c08
deastman <diana.eastman@gmail.com>
2012-11-03 21:46:14 -0400

    Wording changes to About Us page

--- p2.eastmaninteractive.com/views/v_index_about.php
+++ p2.eastmaninteractive.com/views/v_index_about.php
@@ -1,16 +1,19 @@
 <div class="row">
-<h1>About P!NG</h1>
-	<h3>What is P!NG?</h3>
-        <p>P!NG is a place on the interwebs for coders of all flavors to share information related to programming. Like any great application, we're currently in beta and will be 
-        adding more bells and whistles soon. Stay tuned!
-     
-    <h3>Where to start?</h3>  
-    	<p>If you haven't already, create an account - it's free! You can then visit the Dashboard, your very own place to add P!NGs (short posts) about whatever is interesting to you at the moment. Did 		you just write a beautiful function? P!NG it! By following other users, your dashboard will be populated with P!NGs. 
+	<div class="span8">
+		<h1>About P!NG</h1>
+	</div>
+	<div class="span8">
+        <h3>What is P!NG?</h3>
+            <p>P!NG is a place on the interwebs for coders of all flavors to share information related to programming. Like any great application, we're currently in beta and will be 
+            adding more bells and whistles soon. Stay tuned!
+         
+        <h3>Where to start?</h3>  
+            <p>If you haven't already, create an account - it's free! You can then visit the Dashboard, which is your very own place to add P!NGs (short posts) about whatever is interesting to you at the 			moment. Did you just write a beautiful function? P!NG it! By following other users, your dashboard will be populated with P!NGs. 
+            
+            <p>Don't forget to create a profile! This will let fellow P!NGers see your interests. 
         
-        <p>Don't forget to create a profile! This will let fellow P!NGers see your interests. 
-    
-    <h3>How is this different than Stackoverflow?</h3>
-    	<p>While Stackoverflow is oriented around troubleshooting, P!NG aims to be more of a coder's showcase (think <a href="http://dribbble.com/">dribbble</a> for programming). We could
-        also do without the snarkiness on Stackoverflow. 
-
+        <h3>How is this different than Stackoverflow?</h3>
+            <p>While Stackoverflow is oriented around troubleshooting, P!NG aims to be more of a coder's showcase (think <a href="http://dribbble.com/">dribbble</a> for programming). We could
+            also do without the snarkiness on Stackoverflow. 
+	</div>
 </div>
