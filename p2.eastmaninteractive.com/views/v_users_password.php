5a4b8df32d7e53b481b7d36103fae11e55261d18
deastman <diana.eastman@gmail.com>
2012-11-03 20:31:19 -0400

    Updating text on page for forgotten password

--- p2.eastmaninteractive.com/views/v_users_password.php
+++ p2.eastmaninteractive.com/views/v_users_password.php
@@ -1,11 +1,12 @@
 <h1>Forgot Your Password?</h1>
-	<p>Never fear! Enter the email associated with your account below and we'll send you a new one.</p>
+	<p>Never fear! Enter the email associated with your account in the form below and we'll send you a new one - look in your Inbox.</p>
  
 	<form method='POST' action='/users/p_password' id="subForm">
 		<label class="control-label" for="inputEmail">Email</label>
 		<input type="text" id="inputEmail" placeholder="Required Field" name="email" class="email required">
 		<button type="submit" class="btn">Submit</button>
-		</form> 
+	</form> 
+        
 <? if ($error):?>
-Account doesn't exist.
+	<p>We don't have a record for an account with this email address. You can sign up for an account <a href="/index">here</a>.
 <? endif;?>
