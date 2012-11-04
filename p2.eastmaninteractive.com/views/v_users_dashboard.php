ee15c18284f3b9ae2377cbf02bb46b3681801db5
deastman <diana.eastman@gmail.com>
2012-11-03 20:26:03 -0400

    Finalizing dashboard

--- p2.eastmaninteractive.com/views/v_users_dashboard.php
+++ p2.eastmaninteractive.com/views/v_users_dashboard.php
@@ -2,16 +2,18 @@
 
 <div class="row">
 	<div class="span4">
+    
     <!--Area for the user to compose a new P!NG-->
     <?=$compose?>
        
     </div>
     
 	<div class="span8">
-    <h2>Recent P!NGs</h2>
+    	<h2>Recent P!NGs</h2>
     
     <!--If the user hasn't followed anyone or composed any P!NGs there is nothing to see here. Alert to follow other users or compose a P!NG.-->
-    <? if(isset($noposts)):?>
+    
+	<? if(isset($noposts)):?>
 	<div class="alert alert-block">
   		<p>Nothing to see here yet! Compose a new P!NG or browse to follow other users.</p>
     </div>
