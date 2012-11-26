be628461acec70dec5f199ed4abf799f90188398
deastman <diana.eastman@gmail.com>
2012-11-25 19:43:16 -0500

    Initial commit

--- core/libraries/User.php
+++ core/libraries/User.php
@@ -250,11 +250,12 @@ class User {
 		$update = DB::instance(DB_NAME)->update("users", Array("password" => $hashed_password), "WHERE user_id = ".$user_id);
 	
 		# Success
-		if($update) 
+		if($update) {
 			return $new_password;
-		else 
+		}
+		else {
 			return false;
-	
+		}
 	}
 	
 	/*-------------------------------------------------------------------------------------------------
