e39c0e21feda40219637a0f27e3ddd03be15d231
deastman <diana.eastman@gmail.com>
2012-12-19 16:07:21 -0500

    Improved error handling on dashboard page (checking if URL is a valid Youtube link); may want to refine further

--- p4.tutoreo.net/views/v_dashboard_add.php
+++ p4.tutoreo.net/views/v_dashboard_add.php
@@ -1,12 +1,17 @@
 <div class="span9">
-	<h2>Add a Tutorial</h2>
-
-    <form method='POST' action='/dashboard/p_add'>
+	<div class="well">
+    <h2 class="add">Add a Tutorial</h2>
+    <form method='POST' action='/dashboard/p_add' id="addVideo">
         <label class="control-label" for="inputURL">Paste the Youtube URL:</label>
         <input type="text" id="inputURL" name="url" class="required">
         
+        <div class="ghost"><div class="alert alert-error">Sorry, this doesn't look like a valid Youtube URL. We only support Youtube videos at this time, but
+        hope to add more formats soon!
+        </div>
+        </div>
+        
         <label class="control-label" for="inputTitle">Title Your Tutorial:</label>
-        <input type="text" id="inputTitle" name="title" class="required">
+        <input type="text" id="inputTitle" name="title" class="required" value="Required">
         
        <label class="control-label" for="inputTag">Tag Your Tutorial:</label>
        <select name="tag">
@@ -23,19 +28,63 @@
         </select>
          
          <? if(isset($groups)): ?>
-        <label class"control-label" for="inputNewGroup">Add to an Existing Group:</label>   
-        <select name="group_name">
+        <label class"control-label" for="inputNewGroup">Add to a Group:</label>   
+        <select name="group_name" id="group_names" class="addGroup">
         		<? foreach($groups as $group): ?>
                 <option type="text" name="<?=$group['group_name']?>" id="<?=$group['group_name']?>" value="<?=$group['group_name']?>"><?=$group['group_name']?></option>
                 <? endforeach; ?> 	
+                <option type="text" name="group_name" id="group_name" value="new">Create a New Group...</option>
         </select>	
+        
+        <div id="new_group">
+        <label for="inputNewGroup" id="groupNew">New Group Name:</label>
+    	<input type="text" class="text" name="group_name" id="groupNew"/>
+        </div>
+        
         <? else:?>
-        <label class="control-label" for="inputGroup">Create a New Group:</label>
-         <input type="text" id="inputGroup" name="group_name" class="required">
+        <label class="control-label" for="inputGroupNew" id="groupNew">Create a New Group:</label>
+        <input type="text" id="groupNew" name="group_name" class="required" value="group_name">
 		<? endif; ?>
         <br /><br />
         <button type='submit' class="btn">Submit</button>
     </form>
+    </div>
 </div>
 
+<script type="text/javascript">
+$(document).ready(function() {
+ 	$('#new_group').hide();
+ 	
+	$('#group_names').bind('change', function (e) { 
+    
+	if( $('#group_names').val() == 'new') {
+      $('#new_group').show();
+    }
+    else{
+      $('#new_group').hide();
+    }         
+  });
+	
+$("#addVideo").validate();
+	
+function ytVidId(url) {
+    var p = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?(?=.*v=((\w|-){11}))(?:\S+)?$/;
+    return (url.match(p)) ? RegExp.$1 : false;
+}
 
+$('.ghost').hide();
+$('#inputURL').bind("change keyup input", function() {
+
+    var url = $(this).val();
+    if (ytVidId(url) == false) {
+        $('.ghost').show();
+		$(this).val('');
+    } 
+	else {
+	$('.ghost').hide();
+	}
+});
+
+
+});
+</script>
