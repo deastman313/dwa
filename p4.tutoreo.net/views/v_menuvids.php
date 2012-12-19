22b9011b5a2141398d24c4efd0155e578ba563b1
deastman <diana.eastman@gmail.com>
2012-12-19 16:36:58 -0500

    Update video menu

--- p4.tutoreo.net/views/v_menuvids.php
+++ p4.tutoreo.net/views/v_menuvids.php
@@ -2,10 +2,9 @@
 	<div id="leftmenu" data-spy="affix">
 		<div class="well" >
 			<ul class="nav nav-list" >
-        		<h4>Sort &amp; Filter</h4>
+        		<h4>View</h4>
         			<li><a href="/videos/index">Latest Ten Videos</a></li>
             		<li><a href="/videos/groups">Groups</a></li>
-  					<li><a href="/videos/tags">Tags</a></li>
 			</ul>
         </div>
 	</div>
