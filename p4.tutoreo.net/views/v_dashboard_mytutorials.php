e16425c18873e8867c7568fa51c4bb32856a743d
deastman <diana.eastman@gmail.com>
2012-12-19 17:18:44 -0500

    Added alert block markup

--- p4.tutoreo.net/views/v_dashboard_mytutorials.php
+++ p4.tutoreo.net/views/v_dashboard_mytutorials.php
@@ -1,9 +1,11 @@
 <div class="span9">
 	<? if(isset($novideos)): ?>
     	<div class="well">
-        	<h3 class="add">No Tutorials</h3>
-  				<p>You haven't added any tutorials yet. Once you do, they'll show up here!
-       			<br />
+        	<div class="alert alert-block">
+        		<h3 class="add">No Tutorials</h3>
+  					<p>You haven't added any tutorials yet. Once you do, they'll show up here!
+       				<br />
+             </div>
     	</div>
     <? else:?>
 
