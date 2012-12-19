88c1af29ddd994fce103a37fee2b4751bf6451f7
deastman <diana.eastman@gmail.com>
2012-12-19 02:17:03 -0500

    Fixing offset on fixed let navigation bars

--- p4.tutoreo.net/views/v_menu.php
+++ p4.tutoreo.net/views/v_menu.php
@@ -1,10 +1,12 @@
 <div class="span3">
-		<div class="well" data-spy="affix" data-offset-top="70">
-		<ul class="nav nav-list" >
-        <h4>Howdy <?=$user->first_name?>!</h4>
-        	<li><a href="#">My Subscriptions</a></li>
-            <li><a href="/dashboard/add">Add a Tutorial</a></li>
-  			<li><a href="#">My Tutorials</a></li>
-		</ul>
+	<div id="leftmenu" data-spy="affix">
+		<div class="well">
+			<ul class="nav nav-list" >
+        		<h4>Howdy <?=$user->first_name?>!</h4>
+        			<li><a href="#">My Subscriptions</a></li>
+            		<li><a href="/dashboard/add">Add a Tutorial</a></li>
+  				<li><a href="#">My Tutorials</a></li>
+			</ul>
         </div>
+    </div>
 </div>
--- p4.tutoreo.net/views/v_menuvids.php
+++ p4.tutoreo.net/views/v_menuvids.php
@@ -1,10 +1,12 @@
-<div class="span3">
-		<div class="well" data-spy="affix" data-offset-top="70">
-		<ul class="nav nav-list" >
-        <h4>Sort &amp; Filter</h4>
-        	<li><a href="/videos/index">Latest Ten Videos</a></li>
-            <li><a href="/videos/groups">Groups</a></li>
-  			<li><a href="/videos/tags">Tags</a></li>
-		</ul>
+<div class="span3" >
+	<div id="leftmenu" data-spy="affix">
+		<div class="well" >
+			<ul class="nav nav-list" >
+        		<h4>Sort &amp; Filter</h4>
+        			<li><a href="/videos/index">Latest Ten Videos</a></li>
+            		<li><a href="/videos/groups">Groups</a></li>
+  					<li><a href="/videos/tags">Tags</a></li>
+			</ul>
         </div>
+	</div>
 </div>
