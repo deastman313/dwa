c0007a825f43965e061037e4908764aa1a513e35
deastman <diana.eastman@gmail.com>
2012-12-19 22:53:49 -0500

    Changed title of menu link

--- p4.tutoreo.net/views/v_menu.php
+++ p4.tutoreo.net/views/v_menu.php
@@ -3,7 +3,7 @@
 		<div class="well">
 			<ul class="nav nav-list" >
         		<h4>Howdy <?=$user->first_name?>!</h4>
-        			<li><a href="/dashboard/index">My Subscriptions</a></li>
+        			<li><a href="/dashboard/index">Tutorials Followed</a></li>
             		<li><a href="/dashboard/add">Add a Tutorial</a></li>
   				<li><a href="/dashboard/mytutorials">My Tutorials</a></li>
 			</ul>
