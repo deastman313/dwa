64d3d1d8dc046ec0d47d11b1a75bc0dd0f4bd354
deastman <diana.eastman@gmail.com>
2012-12-19 21:52:40 -0500

    Fixed(?) group page

--- p4.tutoreo.net/views/v_videos_groups.php
+++ p4.tutoreo.net/views/v_videos_groups.php
@@ -1,7 +1,12 @@
 <div class="span9">
 	<div class="well">
 		<h3 class="add">Tutoreo Groups</h3>
-    	
+        <p>Click on a group name to view all of the videos in that group.
+        <ul>
+    	<? foreach($group_names as $group_name): ?>
+        	<li><a href="/videos/groupvideos/<?=$group_name['group_name'] ?>" rel="tooltip" title="View all videos in this group"><?=$group_name['group_name'] ?></a></li>
+        <? endforeach; ?>
+        </ul>
     </div>
  </div>
    
