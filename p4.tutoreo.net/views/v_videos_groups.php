719327b4e36596f8c60033f4e17969b2399ace6d
deastman <diana.eastman@gmail.com>
2012-12-19 20:33:58 -0500

    Troubleshooting groups page

--- p4.tutoreo.net/views/v_videos_groups.php
+++ p4.tutoreo.net/views/v_videos_groups.php
@@ -1,10 +1,7 @@
 <div class="span9">
 	<div class="well">
-		<h2>Tutoreo Groups</h2>
-    	<ul>
-    	<? foreach($group_names as $group_name): ?>
-    	 	<li><a href="/videos/groupvideos/<?=$group_name['group_name']?>"><?=$group_name['group_name']?></a></li>
-    	<? endforeach; ?>
-        </ul>
-	</div>
-</div>
+		<h3 class="add">Tutoreo Groups</h3>
+    	
+    </div>
+ </div>
+   
