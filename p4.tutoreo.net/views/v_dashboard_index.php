c60e005dd12c6b0a12de5db07e78543e6972d301
deastman <diana.eastman@gmail.com>
2012-12-19 22:54:05 -0500

    Changed view structure

--- p4.tutoreo.net/views/v_dashboard_index.php
+++ p4.tutoreo.net/views/v_dashboard_index.php
@@ -14,7 +14,29 @@
     
 	<? else: ?>
     
-	<?=$vidquery;?>
+	<div class="span9">
+	<? foreach($videos as $video): ?>
+    	<div class="well">
+    		 	
+			<a class="vote up" rel="tooltip" title="Vote up." href="#" id="<?=$video['video_id']?>"><img src="/assets/images/vote_up.png"/></a>
+            <a class="vote down" rel="tooltip" title="Vote down." href="#" id="<?=$video['video_id']?>"><img src="/assets/images/vote_down.png"/></a>
+            Votes: <span class="<?=$video['video_id']?>"><?=$video['voteCount']?></span>
+            
+            	<div class="title">
+             		<h2><?=$video['title']?></h2>
+                    	Posted <?=Time::time_ago ($video['modified'], $dateto=-1)?> in <?=$video['tag']?>
+                </div>
+					
+                <iframe class="youtube-player" type="text/html" height="390" width="640" src="http://www.youtube.com/embed/<?=$video['youtube_id']?>" frameborder="0"></iframe>
+                    
+                <!-- Show unfollow link -->
+            	<div id="<?=$video['video_id']?>"><button class="btn followButton following">Following</button></div>
+				
+           		 	
+  		</div><!--Close well-->
+	<? endforeach; ?>
+</div><!--Close span9-->
+
     
     <? endif;?>
 </div>
