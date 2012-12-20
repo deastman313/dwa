9f682f5f6eefebece030350ce0cc653c74ae39df
deastman <diana.eastman@gmail.com>
2012-12-19 22:31:44 -0500

    Update groups page

--- p4.tutoreo.net/views/v_videos_groups.php
+++ p4.tutoreo.net/views/v_videos_groups.php
@@ -2,7 +2,7 @@
 	<div class="well">
 		<h3 class="add">Tutoreo Groups</h3>
         <p>Click on a group name to view all of the videos in that group.
-        <ul>
+        <ul class="groups">
     	<? foreach($group_names as $group_name): ?>
         	<li><a href="/videos/groupvideos/<?=$group_name['group_name'] ?>" rel="tooltip" title="View all videos in this group"><?=$group_name['group_name'] ?></a></li>
         <? endforeach; ?>
