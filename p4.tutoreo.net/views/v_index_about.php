ad46acb763d536d5a99fa77451e8cd81b0e78d5f
deastman <diana.eastman@gmail.com>
2012-12-19 21:52:09 -0500

    Added about page

---
+++ p4.tutoreo.net/views/v_index_about.php
@@ -0,0 +1,19 @@
+<div class="span8">
+	<h1>About Tutoreo</h1>
+    	<div class="well">
+        <h3 class="add">What is Tutoreo?</h3>
+            <p>Tutoreo is a simple little utility where you can post and organize tutorial videos. Currently, videos can be organized into groups (e.g., the group, How to Build a Website, might be
+            composed of five different tutorials) and tagged by subject matter. The subject tags don't do much yet -- they mainly help describe what kind of content you can expect if the 
+            title isn't descriptive enough.
+         
+        <h3 class="add">Where to start?</h3>  
+            <p>If you haven't already, create an account. Once your account is up and running you can add tutorials, subscribe to tutorials you find interesting, and vote on existing tutorials. 
+        
+        <h3 class="add">Can you explain the voting system?</h3>
+            <p>Sure! The voting system is a simple tally of upvotes (+1) and downvotes (-1). Each user can only vote once for a particular video, but can toggle her or his vote in either direction. 
+            <p><strong>Example: </strong>Let's say John Doe upvoted "How to Build a Website - Part 1" and Jane Snow downvoted this video. This means the effective vote for the video is 0. If Jane changes her mind and upvotes the video as well, the effective vote will be 2. 
+        	<p>We'd like to make this more sophisticated in the future.
+       </div>
+        
+	</div>
+</div>
