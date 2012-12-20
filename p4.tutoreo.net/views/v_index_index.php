4bd62921083048ffe052e69bddeb168c19aef8f9
deastman <diana.eastman@gmail.com>
2012-12-19 20:33:30 -0500

    Cleaned up index

--- p4.tutoreo.net/views/v_index_index.php
+++ p4.tutoreo.net/views/v_index_index.php
@@ -6,51 +6,17 @@
 </div>
 <? else:?>
 	<div class="span8">
-    	<h1>Latest Tutorials</h1>
-        
-            	<div id="whateverID">
-                <iframe width="640" height="390" frameborder="0" title="YouTube video player" type="text/html" src="http://www.youtube.com/embed/<?=$latest_video['youtube_id']?>?enablejsapi=1">
-                </iframe>
-                <a href="javascript:void callPlayer(&quot;whateverID&quot;,&quot;stopVideo&quot;)">Stop</a>
-                </div>
+    	<h1>Latest Tutorial</h1>
+        	<iframe class="youtube-player" type="text/html" height="340" width="560" src="http://www.youtube.com/embed/<?=$latest_videos['youtube_id']?>" frameborder="0"></iframe> 
     </div>
     
     <div class="span4">
-    	<h2>Sign Up</h2>
-     		<?=$signup?>
-	</div>        
+    	<h1>Sign Up</h1>
+     	<?=$signup?>
+	</div>   
+   
 
 <? endif;?>
-
-
-<script type="text/javascript">
-
-/*
- * @author       Rob W (http://stackoverflow.com/a/7513356/938089
- * @description  Executes function on a framed YouTube video (see previous link)
- *               For a full list of possible functions, see:
- *               http://code.google.com/apis/youtube/js_api_reference.html
- * @param String frame_id The id of (the div containing) the frame
- * @param String func     Desired function to call, eg. "playVideo"
- * @param Array  args     (optional) List of arguments to pass to function func*/
-function callPlayer(frame_id, func, args) {
-    if (window.jQuery && frame_id instanceof jQuery) frame_id = frame_id.get(0).id;
-    var iframe = document.getElementById(frame_id);
-    if (iframe && iframe.tagName.toUpperCase() != 'IFRAME') {
-        iframe = iframe.getElementsByTagName('iframe')[0];
-    }
-    if (iframe) {
-        // Frame exists, 
-        iframe.contentWindow.postMessage(JSON.stringify({
-            "event": "command",
-            "func": func,
-            "args": args || [],
-            "id": frame_id
-        }), "*");
-    }
-}
-</script>
-
 
 
 
